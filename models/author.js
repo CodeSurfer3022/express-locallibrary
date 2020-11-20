7274410b8b9933d88f56a1cef4ee78b0aafcc5c9
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-11-20 15:30:44 +0530

    Add author schema

---
+++ models/author.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema({
+    family_name: {type: String, required: true, maxLength: 100},
+    first_name: {type: String, required: true, maxLength: 100},
+    date_of_birth: {type: Date},
+    date_of_death: {type: Date}
+});
+
+// Virtual for author's name
+AuthorSchema
+    .virtual('name')
+    .get(() => this.family_name + ', ' + this.first_name);
+
+// Virtual for author's lifespan
+AuthorSchema
+    .virtual('lifespan')
+    .get(() => this.date_of_death.getYear() - this.date_of_birth.getYear().toString());
+
+// Virtual for author's URL
+AuthorSchema
+    .virtual('url')
+    .get(() => '/catalog/author/' + this._id);
+
+module.exports = mongoose.model('Author', AuthorSchema);
