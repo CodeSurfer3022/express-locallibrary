dbc0fd3969bead6872567491b69327b7b976eb8f
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-11-26 10:23:29 +0530

    update book list in bookController

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -7,62 +7,70 @@ const async = require('async');
 
 exports.index = function (req, res) {
   async.parallel({
-      book_count: (callback) => {
-          Book.countDocuments({}, callback);
-      },
-      book_instance_count: (callback) => {
-          BookInstance.countDocuments({}, callback);
-      },
-      book_instance_available_count: (callback) => {
-          BookInstance.countDocuments({status: 'Available'}, callback);
-      },
-      author_count: (callback) => {
-          Author.countDocuments({}, callback);
-      },
-      genre_count: (callback) => {
-          Genre.countDocuments({}, callback);
-      }
+    book_count: (callback) => {
+      Book.countDocuments({}, callback);
+    },
+    book_instance_count: (callback) => {
+      BookInstance.countDocuments({}, callback);
+    },
+    book_instance_available_count: (callback) => {
+      BookInstance.countDocuments({status: 'Available'}, callback);
+    },
+    author_count: (callback) => {
+      Author.countDocuments({}, callback);
+    },
+    genre_count: (callback) => {
+      Genre.countDocuments({}, callback);
+    }
   }, (err, results) => {
-      res.render('index', {title: 'Local library Home', error: err, data: results});
+    res.render('index', {title: 'Local library Home', error: err, data: results});
   });
 };
 
 // Display list of all books
-exports.book_list = (req, res) => {
-    res.send('Not implemented: book list');
+exports.book_list = (req, res, next) => {
+  Book.find({}, 'title author')
+    .populate('author')
+    .exec( (err, list_books) => {
+      if (err) {
+        return next(err);
+      }
+      //Successful, so render
+      res.render('book_list', {title: 'Book List', book_list: list_books});
+    });
 };
 
 // Display detail page for a specific book
 exports.book_detail = (req, res) => {
-    res.send('Not implemented: book detail:' + req.params.id);
+  res.send('Not implemented: book detail:' + req.params.id);
 };
 
 // Display Book create form on GET.
 exports.book_create_get = (req, res) => {
-    res.send('Not implemented: book create GET');
+  res.send('Not implemented: book create GET');
 };
 
 // Handle Book create on POST.
 exports.book_create_post = (req, res) => {
-    res.send('Not implemented: book create POST');
+  res.send('Not implemented: book create POST');
 };
 
 // Display book update form on GET.
 exports.book_update_get = (req, res) => {
-    res.send('Not implemented: book update GET');
+  res.send('Not implemented: book update GET');
 };
 
 // Handle book update on POST.
 exports.book_update_post = (req, res) => {
-    res.send('Not implemented: book update POST');
+  res.send('Not implemented: book update POST');
 };
 
 // Display book delete form on GET.
 exports.book_delete_get = (req, res) => {
-    res.send('Not implemented: book delete GET');
+  res.send('Not implemented: book delete GET');
 };
 
 // Handle book delete on POST.
 exports.book_delete_post = (req, res) => {
-    res.send('Not implemented: book delete POST');
+  res.send('Not implemented: book delete POST');
 };
